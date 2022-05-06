4a9f5ebaec131f18a6b97a5d382f9d4812c90929
dzhamBool <dzhamBool@yandex.ru>
2022-05-06 16:37:21 +0300

    Feed and Post viewControllers

--- Navigation/FeedViewController.swift
+++ Navigation/FeedViewController.swift
@@ -6,7 +6,27 @@ class FeedViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .lightGray
+        makeButton()
     }
     
+    private func makeButton() {
+let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
+        button.center = view.center
+        button.setTitle("POST", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .cyan
+        button.addTarget(self, action: #selector(tapAction), for: .touchUpInside)
+        view.addSubview(button)
+    }
+    @ objc private func tapAction() {
+let postPage = PostViewController()
+        present(postPage, animated: true)
+    }
 
+//        let barItem = UIBarButtonItem(title: "POST", style: .plain, target: self, action: #selector(tapAction))
+//        navigationItem. = barItem
+//    }
+//    @objc private func tapAction() {
+//
+//    }
 }
--- Navigation/PostViewController.swift
+++ Navigation/PostViewController.swift
@@ -11,19 +11,11 @@ class PostViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.navigationItem.title = "Your Title"
+        //self.title = "Post #1"
+        view.backgroundColor = .orange
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
