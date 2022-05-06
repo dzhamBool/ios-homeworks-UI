bb7e496cb66fe9c5153fbdf7037510566649b941
dzhamBool <dzhamBool@yandex.ru>
2022-05-06 18:38:08 +0300

    PostViewController + BarButton

--- Navigation/PostViewController.swift
+++ Navigation/PostViewController.swift
@@ -13,6 +13,14 @@ class PostViewController: UIViewController {
         super.viewDidLoad()
         self.navigationItem.title = post?.title
         view.backgroundColor = .orange
+        makeInfoButton()
     }
-
+    private func makeInfoButton() {
+    let barButton = UIBarButtonItem(title: "FORVARD", style: .plain, target: self, action: #selector(tapAction))
+        navigationItem.rightBarButtonItem = barButton
+}
+    @objc private func tapAction() {
+let infoVC = InfoViewController()
+        present(infoVC, animated: true)
+    }
 }
