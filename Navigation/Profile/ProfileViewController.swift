cd02b25303f8265bd0baa86f5a246ce18b8f108c
dzhamBool <dzhamBool@yandex.ru>
2022-05-10 23:11:59 +0300

    Добавлены: Имя, статус, кнопка

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -3,18 +3,16 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .yellow
-        
     }
-
+    
     override func viewWillLayoutSubviews() {
         let profileHeaderView = ProfileHeaderView(frame: self.view.safeAreaLayoutGuide.layoutFrame)
         profileHeaderView.backgroundColor = .lightGray
         view.addSubview(profileHeaderView)
-        
     }
 }
 
