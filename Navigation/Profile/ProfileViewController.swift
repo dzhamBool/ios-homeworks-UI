816ff85e31cb06df648d0454b76bc9deca32344c
dzhamBool <dzhamBool@yandex.ru>
2022-05-14 15:02:01 +0300

    test textField

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -3,16 +3,36 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+
+    let profileHeaderView: ProfileHeaderView = {
+let profileView = ProfileHeaderView()
+        profileView.backgroundColor = .green
+
+return profileView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        view.backgroundColor = .white
+        layout()
+        //view.addSubview(profileHeaderView)
     }
     
     override func viewWillLayoutSubviews() {
-        let profileHeaderView = ProfileHeaderView(frame: self.view.safeAreaLayoutGuide.layoutFrame)
+        profileHeaderView.frame = view.safeAreaLayoutGuide.layoutFrame
         profileHeaderView.backgroundColor = .lightGray
-        view.addSubview(profileHeaderView)
+        //view.addSubview(profileHeaderView)
+    }
+
+    private func layout() {
+        self.view.addSubview(profileHeaderView)
+
+        NSLayoutConstraint.activate([
+            profileHeaderView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            profileHeaderView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            profileHeaderView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+            //profileHeaderView.heightAnchor.constraint(equalToConstant: 40)
+        ])
     }
 }
 
